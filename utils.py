04cf5d17edfa097dc80d7781817ff806972f71e1
mikias-AI <mikiyasabate003@gmail.com>
2025-05-23 18:39:53 +0300

    test

--- utils.py
+++
@@ -1,8 +0,0 @@
-from api import get
-
-def get_todolist_ids(project_id):
-    res = get(f"buckets/{project_id}/todolists.json")
-    if res.status_code == 200:
-        return [(lst['name'], lst['id']) for lst in res.json()]
-    else:
-        return []
